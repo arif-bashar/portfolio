d0b6c3975aff5e5d1270c65f5b36c1e18d7c06e1
Arif Bashar <arifbasharx@gmail.com>
2020-08-08 22:37:24 -0500

    starter files

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "../styles/styles.scss"
+import { PageProps } from "gatsby"
+
+function IndexPage(props: PageProps) {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>Hi there!</p>
+    </>
+  )
+}
+
+export default IndexPage
---
+++ src/pages/page-2.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link, PageProps } from "gatsby"
+
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
+
+function SecondPage(props: PageProps) {
+  return <p>page 2</p>
+}
+
+export default SecondPage
